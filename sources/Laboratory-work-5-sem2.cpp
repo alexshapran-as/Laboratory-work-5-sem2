9359b485b1146094ba15afc263b03f18ac73ca56
Alex Shapran <shapran400@gmail.com>
2017-04-14 18:35:12 +0300

    Update Laboratory-work-5-sem2.cpp

--- sources/Laboratory-work-5-sem2.cpp
+++ sources/Laboratory-work-5-sem2.cpp
@@ -1,6 +1,6 @@
 #include "Laboratory-work-5-sem2.hpp"
 
- template <typename BinarySearchTree> int BinarySearchTree::Add(unsigned int new_data, BinarySearchTree<unsigned int> *&root)
+ template <typename Type> int BinarySearchTree::Add(unsigned int new_data, BinarySearchTree<unsigned int> *&root)
 {
 	if (!root)
 	{
