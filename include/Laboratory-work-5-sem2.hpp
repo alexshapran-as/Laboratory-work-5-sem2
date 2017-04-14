553f05b48642788165e6c82d93e654557962fc11
Alex Shapran <shapran400@gmail.com>
2017-04-14 16:58:46 +0300

    Update Laboratory-work-5-sem2.hpp

--- include/Laboratory-work-5-sem2.hpp
+++ include/Laboratory-work-5-sem2.hpp
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 using namespace std;
  
-
+template <class Type>
 class BinarySearchTree
 {
 
 public: 
 	BinarySearchTree *parent; 
-	unsigned int data;
-	int Add(unsigned int new_data, BinarySearchTree *&root);
+	Type data;
+	int Add(Type new_data, BinarySearchTree *&root);
 	int print(int check1, BinarySearchTree *root);
 	int Search(int check2, BinarySearchTree *root);
 	int Deletion(int check3, BinarySearchTree *root);
