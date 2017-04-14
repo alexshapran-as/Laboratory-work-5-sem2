b29fb92030c683bc502999034b3b036d11bff7b6
Alex Shapran <shapran400@gmail.com>
2017-04-14 18:19:33 +0300

    Update Laboratory-work-5-sem2.cpp

--- sources/Laboratory-work-5-sem2.cpp
+++ sources/Laboratory-work-5-sem2.cpp
@@ -1,6 +1,6 @@
 #include "Laboratory-work-5-sem2.hpp"
 
- int Add(unsigned int new_data, BinarySearchTree *&root)
+ int BinarySearchTree::Add(unsigned int new_data, BinarySearchTree *&root)
 {
 	if (!root)
 	{
